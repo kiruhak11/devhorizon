6cd44b6e10a06084610b70674cb6d416066cf323
kiruhak <kiruhak1991@gmail.com>
2024-12-03 21:19:34 +0700

    device

--- app.vue
+++ app.vue
@@ -11,14 +11,14 @@
 <script setup lang="ts">
 import axios from "axios";
 
-useSeoMeta({
-  titleTemplate: (title) =>
-    title
-      ? `${title} | devHorizon`
-      : "devHorizon  - Добро пожаловать в DevHorizon",
-  viewport:
-    'name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"',
-});
+// useSeoMeta({
+//   titleTemplate: (title) =>
+//     title
+//       ? `${title} | devHorizon`
+//       : "devHorizon  - Добро пожаловать в DevHorizon",
+//   viewport:
+//     'name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover"',
+// });
 
 const isReady = ref<boolean>(false);
 

d03c4c4bbb4b8a57fde898ac7bd03e05dfb97f6f
kiruhak <kiruhak1991@gmail.com>
2024-12-04 18:54:42 +0700

    sql

--- prisma/migrations/20241122135212_add_courses/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Course" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "title" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "description" TEXT NOT NULL,
-    "theme" TEXT NOT NULL
-);
